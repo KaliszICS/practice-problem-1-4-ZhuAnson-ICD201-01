37a3f81bd840c1ca1ce6d712b9b2fe48a11255c9
ZhuAnson-ICD201-01 <anson.zhu@student.tdsb.on.ca>
2024-09-19 13:52:53 +0000

    commit

--- PP1_4.py
+++ PP1_4.py
@@ -1,16 +1,22 @@
 
 
 def q1():
-  #Write Assignment code here
+  word = input("Input a word: ")
+  print (word)
 
 def q2():
-  #Write Assignment code here
+  name = input("Input your name: ")
+  print ("Hello " + name)
 
 def q3():
-  #Write Assignment code here
+  firstName = input("Input your first name: ")
+  lastName = input ("Input your last name: ")
+  print (f"{lastName} {firstName}")
 
 def q4():
-  #Write Assignment code here
+  studentOne = input("Input a student: ")
+  studentTwo = input("Input another student: ")
+  print (f"Your students are {studentOne} and {studentTwo}")
 
 #Do not edit code below this comment
 
